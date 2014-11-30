e07770a1e4fd9c6ed7b2ac4910f4dbb1b94f6c56
Aleksey <yurchakaleksey@gmail.com>
2014-11-30 22:32:59 +0300

    Create repository methods to fetch posts, total posts and category based on slug

--- BlogSolution/BlogProject.Core/IBlogRepository.cs
+++ BlogSolution/BlogProject.Core/IBlogRepository.cs
@@ -11,5 +11,9 @@ namespace BlogProject.Core
     {
         IList<Post> Posts(int pageNo, int pageSize);
         int TotalPosts();
+
+        IList<Post> PostsForCategory(string categorySlug, int pageNo, int pageSize);
+        int TotalPostsForCategory(string categorySlug);
+        Category Category(string categorySlug);
     }
 }
