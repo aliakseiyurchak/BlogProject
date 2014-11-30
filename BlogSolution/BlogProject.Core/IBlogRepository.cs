df2e4b083bd016be2e985f75c2994adccf3ab2aa
Aleksey <yurchakaleksey@gmail.com>
2014-11-30 22:58:02 +0300

    Create a repository method to return post based upon year, month and url-slug

--- BlogSolution/BlogProject.Core/IBlogRepository.cs
+++ BlogSolution/BlogProject.Core/IBlogRepository.cs
@@ -22,5 +22,7 @@ namespace BlogProject.Core
 
         IList<Post> PostsForSearch(string search, int pageNo, int pageSize);
         int TotalPostsForSearch(string search);
+
+        Post Post(int year, int month, string titleSlug);
     }
 }
