b983a11952376cc12022650c6d1b706aeb6d91e5
Aleksey <yurchakaleksey@gmail.com>
2014-11-30 23:06:33 +0300

    Create a view model for categories

---
+++ BlogSolution/BlogProject/Models/WidgetViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BlogProject.Core;
+using BlogProject.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogProject.Models
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IBlogRepository blogRepository)
+        {
+            Categories = blogRepository.Categories();
+        }
+
+        public IList<Category> Categories { get; private set; }
+    }
+}
