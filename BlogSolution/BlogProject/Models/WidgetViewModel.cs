9605f758fa1444af699b92eac112b0ba044d879b
Aleksey <yurchakaleksey@gmail.com>
2014-11-30 23:17:09 +0300

    Update the WidgetViewModel

--- BlogSolution/BlogProject/Models/WidgetViewModel.cs
+++ BlogSolution/BlogProject/Models/WidgetViewModel.cs
@@ -12,8 +12,10 @@ namespace BlogProject.Models
         public WidgetViewModel(IBlogRepository blogRepository)
         {
             Categories = blogRepository.Categories();
+            Tags = blogRepository.Tags();
         }
 
         public IList<Category> Categories { get; private set; }
+        public IList<Tag> Tags { get; private set; }
     }
 }
