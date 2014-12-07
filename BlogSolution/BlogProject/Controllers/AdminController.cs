00f2e8b5bb1e8101234eab4f81b8bc5b4e610c24
Aleksey <yurchakaleksey@gmail.com>
2014-12-07 15:30:16 +0300

    Add AdminController.cs and action for login

---
+++ BlogSolution/BlogProject/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProject.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Manage");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
